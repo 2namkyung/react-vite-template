ef25bae13c8bb6bd8e3326cd070114bcdf61f49c
2namkyung <iddbxk@daum.net>
2022-06-09 10:11:33 +0900

    react vite template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React and Tailwind with Vite
+      </h1>
+    </div>
+  );
+}
+
+export default App;
